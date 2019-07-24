ddf85c05e00d616d205227dd1be5437eb70557a9
Volkya <matiasdylanc@gmail.com>
2019-07-24 18:16:02 -0300

    Bot created

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const { Client } = require('discord.js');
+const client = new Client();
+
+// Token bot
+client.login('NjAzNDQ4MTM4OTUwMjQ2NDEw.XTi5FQ.KNp-J2uy0zQViDMSSKI4TZOfARw');
+
+client.on('ready', () => {
+    console.log('Bot is ready'); 
+})
+
+client.on('message', msg => {
+    if (msg.content === '!ping') {
+      msg.reply('Pong!');
+    }
+  });
+
+ 
