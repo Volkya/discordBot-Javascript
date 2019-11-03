a4ada8bdfce89ec62c867682c5c30b679eccd29b
Volkya <matiasdylanc@gmail.com>
2019-11-03 17:50:57 -0300

    token agregado, a espera de comandos

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { Client } = require('discord.js');
 const client = new Client();
 
 // Token bot
-client.login('NjAzNDQ4MTM4OTUwMjQ2NDEw.XTi5FQ.KNp-J2uy0zQViDMSSKI4TZOfARw');
+client.login('NjAzNDQ4MTM4OTUwMjQ2NDEw.Xb89Vw.fablxHuxO4vNK3rqq112Ex22-j8'); // 8 == admin
 
 client.on('ready', () => {
     console.log('Bot is ready'); 
@@ -13,7 +13,7 @@ client.on('message', msg => {
       msg.reply('Pong!');
     }
 
-    if (msg.content === '!Rules'){
+    if (msg.content === '!Reglas'){
       msg.reply('#Reglamento')
     }
   });
